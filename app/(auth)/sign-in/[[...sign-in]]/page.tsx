7d8b0e37bb56d2c116fbef96a8d13bc3c66b4e55
ichbinnahid <nahid.zarbiyev@gmail.com>
2024-02-18 02:22:48 +0400

    handle auth with clerk

---
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,8 @@
+import { SignIn } from "@clerk/nextjs";
+import React from "react";
+
+const SignUpInPage = () => {
+  return <SignIn />;
+};
+
+export default SignUpInPage;
---
+++ app/(root)/page.tsx
@@ -0,0 +1,8 @@
+import { UserButton } from "@clerk/nextjs";
+import React from "react";
+
+const HomePage = () => {
+  return <div>home</div>;
+};
+
+export default HomePage;
